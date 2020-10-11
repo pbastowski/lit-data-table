4432e346f3d34d36bac578c801e582686bc7cfc5
Paul Bastowski <pbastowski@gmail.com>
2020-10-11 21:46:30 +0200

    add scss support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'font-awesome/css/font-awesome.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import './main.css'
+import './main.scss'
 
 import { render, computed } from './libs.js'
 import App from './App.js'
