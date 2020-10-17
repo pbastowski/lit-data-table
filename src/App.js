8e1997be35804e4727ad79cc1cf342a244012e61
Paul Bastowski <pbastowski@gmail.com>
2020-10-17 16:36:00 +0200

    simplify

--- src/App.js
+++ src/App.js
@@ -6,45 +6,44 @@ import http from 'axios'
 // import { useState } from './libs.js'
 // import { html, virtual } from 'haunted'
 
-const state = {
-    columns: [
-        {
-            field: 'a',
-            label: 'AAA',
-            align: 'left',
-            iconPrefix: 'fa fa-circle text-warning',
-            iconSuffix: 'fa fa-square text-success',
-            sortable: false
-        },
-        {
-            field: 'b',
-            label: 'The big B',
-            align: 'center',
-            sortable: true
-            // headerClass: 'bg-primary'
-            // itemClass: 'bg-warning'
-        },
-        {
-            field: 'c',
-            label: 'C it is',
-            align: 'right',
-            sortable: true
-        }
-    ],
-    data: [
-        { a: 1, b: 'lalala', c: 9 },
-        { a: 2, b: 'lazcv lala', c: 42 },
-        { a: 3, b: 'lalasfxala', c: 1 },
-        { a: 4, b: 'lalz cxala', c: 42 },
-        { a: 5, b: 'lalala', c: 8 },
-        { a: 6, b: 'lalfhgj fala', c: 23 },
-        { a: 7, b: 'lalasxsla', c: 42 },
-        { a: 8, b: 'lala53 3la', c: 5 },
-        { a: 9, b: 'lalagb la', c: 42 },
-        { a: 10, b: 'lasdgdf lala', c: 742 },
-        { a: 11, b: 'lala7978 la', c: 6 }
-    ]
-}
+const columns = [
+    {
+        field: 'a',
+        label: 'AAA',
+        align: 'left',
+        iconPrefix: 'fa fa-circle text-warning',
+        iconSuffix: 'fa fa-square text-success',
+        sortable: false
+    },
+    {
+        field: 'b',
+        label: 'The big B',
+        align: 'center',
+        sortable: true
+        // headerClass: 'bg-primary'
+        // itemClass: 'bg-warning'
+    },
+    {
+        field: 'c',
+        label: 'C it is',
+        align: 'right',
+        sortable: true
+    }
+]
+
+const data = [
+    { a: 1, b: 'lalala', c: 9 },
+    { a: 2, b: 'lazcv lala', c: 42 },
+    { a: 3, b: 'lalasfxala', c: 1 },
+    { a: 4, b: 'lalz cxala', c: 42 },
+    { a: 5, b: 'lalala', c: 8 },
+    { a: 6, b: 'lalfhgj fala', c: 23 },
+    { a: 7, b: 'lalasxsla', c: 42 },
+    { a: 8, b: 'lala53 3la', c: 5 },
+    { a: 9, b: 'lalagb la', c: 42 },
+    { a: 10, b: 'lasdgdf lala', c: 742 },
+    { a: 11, b: 'lala7978 la', c: 6 }
+]
 
 export default props => {
     const [showTable, setShowTable] = useState(true)
@@ -117,12 +116,12 @@ export default props => {
         </button>
         ${showTable &&
         DataTable({
-            columns: state.columns,
-            data: JSON.parse(JSON.stringify(state.data)),
+            columns: columns,
+            data: JSON.parse(JSON.stringify(data)),
             // getData: ({ page, pageSize }) => {
             //     // console.log('GET DATA:')
-            //     return Promise.resolve(state.data)
-            //     return Promise.resolve(state.data.slice((page - 1) * pageSize, page * pageSize))
+            //     return Promise.resolve(data)
+            //     return Promise.resolve(data.slice((page - 1) * pageSize, page * pageSize))
             // },
             // recordCount: 11,
 
